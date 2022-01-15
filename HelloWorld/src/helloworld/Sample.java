94b760110a73cd4a56ee75ae603c45325d43ac55
miya <miya@miya.com>
2022-01-15 16:57:28 +0900

    Sample

--- HelloWorld/src/helloworld/Sample.java
+++ HelloWorld/src/helloworld/Sample.java
@@ -3,11 +3,8 @@ package helloworld;
 public class Sample {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("Main Branch.");
-=======
 		System.out.println("Sub Branch.");
->>>>>>> refs/heads/sub1
 	}
 
 }
