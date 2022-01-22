a13e196b418bb40f2f5ffbdebcbb5cf5e63dca50
miya <miya@miya.com>
2022-01-22 20:12:29 +0900

    Revert "Sample2 Modify (Mistake 2nd !)"
    This reverts commit 2eb4703b047779a24f2ff1a75e0b4107ced982ae.

--- HelloWorld/src/helloworld/Sample2.java
+++ HelloWorld/src/helloworld/Sample2.java
@@ -5,7 +5,6 @@ public class Sample2 {
 	public static void main(String[] args) {
 		System.out.println("Sample2 Initial.");
 		System.out.println("Sample2 Modify (Mistake 1st !). ");
-		System.out.println("Sample2 Modify (Mistake 2nd !). ");
 	}
 
 }
