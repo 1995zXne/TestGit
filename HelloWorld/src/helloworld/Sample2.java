2eb4703b047779a24f2ff1a75e0b4107ced982ae
miya <miya@miya.com>
2022-01-22 20:06:55 +0900

    Sample2 Modify (Mistake 2nd !)

--- HelloWorld/src/helloworld/Sample2.java
+++ HelloWorld/src/helloworld/Sample2.java
@@ -5,6 +5,7 @@ public class Sample2 {
 	public static void main(String[] args) {
 		System.out.println("Sample2 Initial.");
 		System.out.println("Sample2 Modify (Mistake 1st !). ");
+		System.out.println("Sample2 Modify (Mistake 2nd !). ");
 	}
 
 }
